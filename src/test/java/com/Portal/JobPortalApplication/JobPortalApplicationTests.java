d0c0821b900eebea3bdcb4b4cb8e2801d6ab8245
Nirav <khristinirav111@gmail.com>
2026-06-22 16:40:08 +0530

    Job portal
    Job portal

---
+++ src/test/java/com/Portal/JobPortalApplication/JobPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Portal.JobPortalApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
