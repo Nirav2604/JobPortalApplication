d8ef9c03f692a6be1458f57741ade82633b62e3a
Nirav <khristinirav111@gmail.com>
2026-06-25 16:56:19 +0530

    Created CandidateProfile Service Implmentation.all 3 methods create,get and update are implemented here.

---
+++ src/main/java/com/Portal/JobPortalApplication/service/Implementation/CandidateServiceImpl.java
@@ -0,0 +1,52 @@
+package com.Portal.JobPortalApplication.service.Implementation;
+
+import com.Portal.JobPortalApplication.dto.CandidateProfileRequest;
+import com.Portal.JobPortalApplication.entity.CandidateProfile;
+import com.Portal.JobPortalApplication.entity.User;
+import com.Portal.JobPortalApplication.repository.CandidateProfileRepository;
+import com.Portal.JobPortalApplication.repository.UserRepository;
+import com.Portal.JobPortalApplication.service.CandidateService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CandidateServiceImpl implements CandidateService {
+
+    private final UserRepository userRepository;
+    private final CandidateProfileRepository candidateProfileRepository;
+
+    @Override
+    public CandidateProfile createProfile(String email, CandidateProfileRequest candidateProfileRequest) {
+        User user = userRepository.findByEmail(email).orElseThrow();
+
+        CandidateProfile candidateProfile = CandidateProfile.builder()
+                .phone(candidateProfileRequest.getPhone())
+                .skills(candidateProfileRequest.getSkills())
+                .experience(candidateProfileRequest.getExperience())
+                .education(candidateProfileRequest.getEducation())
+                .user(user)
+                .build();
+
+        return candidateProfileRepository.save(candidateProfile);
+    }
+
+    @Override
+    public CandidateProfile getProfile(String email) {
+        User user = userRepository.findByEmail(email).orElseThrow();
+        return candidateProfileRepository.findByUser(user).orElseThrow();
+    }
+
+    @Override
+    public CandidateProfile UpdateProfile(String email, CandidateProfileRequest candidateProfileRequest) {
+        User user = userRepository.findByEmail(email).orElseThrow();
+        CandidateProfile candidateProfile= candidateProfileRepository.findByUser(user).orElseThrow();
+
+        candidateProfile.setPhone(candidateProfileRequest.getPhone());
+        candidateProfile.setSkills(candidateProfileRequest.getSkills());
+        candidateProfile.setExperience(candidateProfileRequest.getExperience());
+        candidateProfile.setEducation(candidateProfileRequest.getEducation());
+
+        return candidateProfileRepository.save(candidateProfile);
+    }
+}
