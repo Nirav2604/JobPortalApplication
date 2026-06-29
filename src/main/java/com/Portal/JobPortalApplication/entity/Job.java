fc390bc36d29d1061a0665d535c89b1c3713f719
Nirav <khristinirav111@gmail.com>
2026-06-29 15:11:39 +0530

    created an job entity

---
+++ src/main/java/com/Portal/JobPortalApplication/entity/Job.java
@@ -0,0 +1,37 @@
+package com.Portal.JobPortalApplication.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Job {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(length = 3000)
+    private String description;
+
+    private String location;
+
+    private Double salary;
+
+    private Integer experienceRequired;
+
+    private String jobType;
+
+    private LocalDateTime postedDate;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private Company company;
+}
