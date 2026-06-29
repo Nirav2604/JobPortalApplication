21f19168d130b2f0aa3c16e386b5f0d17e8b173c
Nirav <khristinirav111@gmail.com>
2026-06-29 14:19:06 +0530

    Created a Company Repository file.

---
+++ src/main/java/com/Portal/JobPortalApplication/repository/CompanyRepository.java
@@ -0,0 +1,11 @@
+package com.Portal.JobPortalApplication.repository;
+
+import com.Portal.JobPortalApplication.entity.Company;
+import com.Portal.JobPortalApplication.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CompanyRepository extends JpaRepository<Company,Long> {
+    Optional<Company> findByRecruiter(User recruiter);
+}
