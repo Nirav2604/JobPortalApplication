099a4332a9e7c4b1bc03cc93ce063719fa614745
Nirav <khristinirav111@gmail.com>
2026-06-29 15:13:24 +0530

    created an job entity's Repository.

---
+++ src/main/java/com/Portal/JobPortalApplication/repository/JobRepository.java
@@ -0,0 +1,14 @@
+package com.Portal.JobPortalApplication.repository;
+
+import com.Portal.JobPortalApplication.entity.Company;
+import com.Portal.JobPortalApplication.entity.Job;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface JobRepository extends JpaRepository<Job,Long> {
+
+    List<Job> findByCompany(Company company);
+    List<Job> findByTitle(String title);
+
+}
