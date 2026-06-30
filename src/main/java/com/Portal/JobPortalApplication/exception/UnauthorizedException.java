b0b1a20c0f87db82a870d648959abbe754fd921a
Nirav <khristinirav111@gmail.com>
2026-06-30 14:02:45 +0530

    Created UnauthorizedException class file.

---
+++ src/main/java/com/Portal/JobPortalApplication/exception/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.Portal.JobPortalApplication.exception;
+
+public class UnauthorizedException extends RuntimeException{
+    public UnauthorizedException(String message){
+        super(message);
+    }
+}
