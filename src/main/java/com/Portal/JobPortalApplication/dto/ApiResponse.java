a95c94a7c0027a3f3bfd60d0be3602db4da320ea
Nirav <khristinirav111@gmail.com>
2026-06-30 13:59:14 +0530

    Created ApiResponse DTO class.

---
+++ src/main/java/com/Portal/JobPortalApplication/dto/ApiResponse.java
@@ -0,0 +1,16 @@
+package com.Portal.JobPortalApplication.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+
+    private boolean success;
+    private String message;
+    private T data;
+
+}
